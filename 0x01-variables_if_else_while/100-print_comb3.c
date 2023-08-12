668d60e57572cf7e1746d459444aa6df18a598ef
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-12 15:11:58 -0700

    Task has been completed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,20 @@
 
 int main(void)
 {
-	int num1 = '0';
+	int num1 = 0;
 	int num2;
 
-	while (num1 <= '9')
+	while (num1 <= 9)
 	{
-		num2 = '0';
-		while (num2 <= '9')
+		num2 = 0;
+		while (num2 <= 9)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
-				putchar(num1);
-				putchar(num2);
+				putchar(num1 + 48);
+				putchar(num2 + 48);
 
-				if (num1 + num2 != '17')
+				if (num1 + num2 != 17)
 				{
 					putchar(',');
 					putchar(' ');
