31323f07a10d7521ca319dcc0f48444c13e8263f
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-12 13:35:22 -0700

    Task has been completed

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,10 @@
 
 int main(void)
 {
-	int i = 48;
+	int i;
 
-	while (i < 58)
-	{
-		putchar(i);
-		i++;
-	}
-	putchar("\n");
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
 	return (0);
 }
