fef81e8a955a77ceffe41f6e9b0aa2afc73a48f4
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-12 10:31:17 -0700

    Task has been completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Decription: Program prints numbers.
+ *
+ * Return: Always - (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
