87bd9dccf4f3699d76470fb97e07325d32dd000f
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-15 14:39:30 -0700

    Task has been completed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,17 @@
 /**
  * main - Entry point
  *
- * Decription: Program prints the alphabet, in lowercase,using the _putchar.
+ * print_alphabet(Program prints the alphabet, in lowercase,using the _putchar).
  *
- * Return: Always - (Success)
+ * Return: voidd
 */
 
 void print_alphabet(void)
 {
-	int letter;
+	int alphabet;
 
-	for (letter = 'a'; letter <= 'z' ; letter++)
-		_putchar(letter);
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+		_putchar(alphabet);
 
 	_putchar('\n');
 }
