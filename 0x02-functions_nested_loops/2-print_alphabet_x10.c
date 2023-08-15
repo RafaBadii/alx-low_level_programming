569b3c0df16c5af3d1dbec42331208fbf83bc9bc
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-15 15:07:04 -0700

    Task has been completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#inclue "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet (a-z) 10 times using putchar.
+ *
+ * Return: void.
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+
+}
