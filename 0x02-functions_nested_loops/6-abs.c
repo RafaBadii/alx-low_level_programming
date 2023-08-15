779211b8bd597c0c533be45d40444cf115ef8672
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-15 16:43:57 -0700

    Task has been completed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer.
+ *
+ * Return: Always - 0 (Success)
+*/
+
+int _abs(int n)
+{
+
+	printf("%d\n", n);
+	return (0);
+}
