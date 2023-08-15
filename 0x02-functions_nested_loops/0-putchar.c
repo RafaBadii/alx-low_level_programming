98d756d74bbf83997fcaff77533ea6e79d6df765
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-15 10:30:17 -0700

    Task has been completed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Decription: A C program prints _putchar
+ *
+ * Return: Always - (Success)
+*/
 
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; i < 8 ; i++)
+		putchar('str[i]');
+
+	putchar('\n');
 	return (0);
 }
