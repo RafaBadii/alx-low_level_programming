32ae891e6f35bf36eee5117b78d5625fedc8bed2
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-16 11:17:11 -0700

    Task has been completed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 /* there is no increment for the varaiablem, i++; is missing */
