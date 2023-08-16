e0cb940a02e8db4ec389952403dd57e478e523bb
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-15 18:19:06 -0700

    Task has been completed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,9 @@
 
 /**
  * times_table - Print the 9 times table, starting with 0
- */
+ *
+ * Return: void.
+*/
 
 void times_table(void)
 {
@@ -10,22 +12,25 @@ void times_table(void)
 	int multiply;
 	int answer;
 
-		for (number = 0; number < 10; number++)
+	for (number = 0; number < 10; number++)
+	{
+		_putchar(48);
+
+		for (multiply = 1; multiply <= 9; multiply++)
 		{
-			_putchar(48);
-			for (multiply = 1; multiply <= 9; multiply++)
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+
+			answer = number * multiply;
+
+			if (answer <= 9)
 				_putchar(' ');
 
-				answer = number * multiply;
-
-				if (answer <= 9)
-					_putchar(' ');
-				else
-					_putchar((answer / 10) + 48);
+			else
+			{	_putchar((answer / 10) + 48);
 					_putchar((answer % 10) + 48);
 			}
+		}
 			_putchar('\n');
 		}
 }
