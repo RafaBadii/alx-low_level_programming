8a4419a4f41ce0106cd07035e28755459b86bb68
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-17 13:44:57 -0700

    Task has been completed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,10 +8,14 @@
 
 void print_line(int n)
 {
-	if (n > 0)
-	{
-		_putchar(95, n);
-	}
+	if (n <= 0)
+		_putchar('\n');
+
 	else
-	_putchar('\n');
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
 }
