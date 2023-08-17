ba7feaca941acfeabdccc9774c0c5b962f1d78bc
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-17 13:50:16 -0700

    Task has been completed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,19 +3,20 @@
 /**
  * print_line - function that draws a straight line in the terminal
  * @n: the number of times the character _ should be printed
- * Return: Print _ if n is greater than 0, new line if equal or less than 0
 */
 
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 		_putchar('\n');
 
 	else
-		int i;
-
+	{
 		for (i = 1; i <= n; i++)
 		{
 			_putchar(95);
 		}
+	}
 }
