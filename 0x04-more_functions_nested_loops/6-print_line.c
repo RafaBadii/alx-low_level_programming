97dcd0759b59a4635def81dcf117cc502dbf56cd
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-17 13:53:03 -0700

    Task has been completed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,7 @@
 /**
  * print_line - function that draws a straight line in the terminal
  * @n: the number of times the character _ should be printed
+ * Return: print '_' if n is greater than 0, otherwise print new line
 */
 
 void print_line(int n)
@@ -16,7 +17,7 @@ void print_line(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			_putchar(95);
+			_putchar('_');
 		}
 	}
 }
