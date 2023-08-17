bfb45cd629cdfcf29b3d85db866c29a7e401e71e
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-17 10:14:35 -0700

    Task has been completed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from (0-9)
+ * @i: int value
+ * return: void
+*/
+
+void print_numbers
+{
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+}
