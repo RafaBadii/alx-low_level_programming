69c187d9d87af24310f71c9c3b3dac4780299f0a
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-21 15:13:51 -0700

    Task has been completed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - function returns the length of a string
+ * @s: char pointer 
+ * Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; *s++)
+	{
+		count++;
+	}
+	return (count);
+}
