c600a28c1ad2a8fefea511386b94d1ca7327b52b
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-24 09:47:54 -0700

    Task has been completed

---
+++ 0x06-pointers_arrays_stringsls/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - function concatenate two strings
+ * @dest: char string 1
+ * @src: char string 2
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, x;
+
+	/* find the size of dest */
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[i] = src[x];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
