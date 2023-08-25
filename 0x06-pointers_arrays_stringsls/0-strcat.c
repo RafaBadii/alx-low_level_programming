e84f67e868bda168a9b08dc6d310afe7f484f03f
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-25 06:46:29 -0700

    Task has been completed

--- 0x06-pointers_arrays_stringsls/0-strcat.c
+++ 0x06-pointers_arrays_stringsls/0-strcat.c
@@ -15,9 +15,10 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (x = 0; src[x]; x++)
+	for (x = 0; src[x] != '\0'; x++)
 	{
 		dest[i++] = src[x];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
