c2ae4ec03234feb77ae39f73b585829cf57acff6
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-25 07:00:42 -0700

    Task has been completed

--- 0x06-pointers_arrays_stringsls/1-strncat.c
+++ 0x06-pointers_arrays_stringsls/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate two strings
- * @dest: type char str
- * @src: type char str
+ * _strncat - function concatenate two strings
+ * @dest: char string
+ * @src: char string
  * @n: number of elements to concatenate in
  * Return: pointer to resulting `dest`dest
  */
@@ -13,11 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	int a, b;
 
 	for (a = 0; dest[a] != '\0'; a++)
-	{
-	}
+		;
+
 	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
 	{
 		dest[a] = src[b];
 	}
+
 	return (dest);
 }
