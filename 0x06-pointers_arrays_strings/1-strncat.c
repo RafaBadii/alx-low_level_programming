947b0741ab2453ec9784419d3154e43ffc31af03
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-27 14:28:40 -0700

    Task has been completed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,10 +16,9 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; dest[a] != '\0'; a++)
 		;
 
-	for (b = 0; src[b] != '\0' && n > 0; b++, n--)
+	for (b = 0; b < n && src[b] != '\0'; b++)
 	{
-		dest[a] = src[b];
-		a++;
+		dest[a + b] = src[b]; /* add the string of src */ 
 	}
 
 	return (dest);
