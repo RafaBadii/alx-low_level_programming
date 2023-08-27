d3718aab036a347efb1177802fbdce88ef9c5c12
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-27 15:50:07 -0700

    Task has been completed

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Function that reverse the content of an array of int
+ * @string: string
+ * Return: string str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	/* switch from lower to uppercase */
+	/*in ASCII 97 - 65 = 32 */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			/* switch to upper */
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
