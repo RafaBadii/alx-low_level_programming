1e715e044135aa6c36f4f9622c7f61f5718ccdc4
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-28 16:35:23 -0700

    Task has been completed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strchr - Locates a character in a string
@@ -12,7 +11,7 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
