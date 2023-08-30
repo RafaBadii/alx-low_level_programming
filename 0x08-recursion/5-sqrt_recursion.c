88250ff80f35cd303ade0983d4ee2ad365029cd9
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-30 15:30:39 -0700

    Task has been completed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,11 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the natural sqr root number.
- * @n: integer
- * Return: return -1 If n not sqt root then
-*/
-
-int funct(int x, int y);
-int _sqrt_recursion(int n)
-{
-	return (funct(n, 1));
-}
-
-/**
  * funct - Allows operation of _sqrt_recursion
  * @x: int that define sqrt root
  * @y: int that compares x
  * Return: sqrt int or -1 if not int
- */
+*/
 
 int funct(int x, int y)
 {
@@ -26,8 +14,23 @@ int funct(int x, int y)
 	sqrt = y * y;
 	if (sqrt == x)
 		return (y);
+
 	else if (sqrt < x)
 		return (funct(x, y + 1));
+
 	else
 		return (-1);
+}
+
+
+/**
+ * _sqrt_recursion - function that returns the natural sqr root number.
+ * @n: integer
+ * Return: return -1 If n not sqt root then
+*/
+
+
+int _sqrt_recursion(int n)
+{
+	return (funct(n, 1));
 }
