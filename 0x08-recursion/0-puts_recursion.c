ad9cb847dd2749a375814f5440dc2343c5ebec20
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-08-30 14:53:20 -0700

    Task has been completed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _puts_recursion - Prints a string followed by a new line.
  * @s: pointer to string
- * Return: void 
+ * Return: void
 */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	
+
 		_putchar('\n');
 	else
 	{
