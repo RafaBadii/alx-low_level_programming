34cc8feb3bc34110867af1b7e90d64bf6f993562
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-09-05 14:37:26 -0700

    Task has been completed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _strdup - returns *p to new alloc spc in mem, contains cp of str as par.
- * @str: type char memory value.
- * Return: Null if fails or if srt is NULL.
- */
+ * _strdup - Function returns *ptr to new alloc space in memory.
+ * @str: char memory value.
+ * Return: Null if fails or if str is NULL.
+*/
+
 char *_strdup(char *str)
 {
-	char *p;
-	unsigned int x, y;
+	char *ptr;
+	int size, i;
 
-	if (str == NULL)
+	if (str == 0)
 		return (NULL);
 
-	for (x = 0; str[x] != '\0'; x++)
+	/* Size of the string */
+	for (size = 0; str[size] != '\0'; size++)
 		;
 
-	p = malloc(x * sizeof(*p) + 1);
-	if (p == NULL)
+	/* +1 to include the last str character */
+	ptr = malloc(size * sizeof(char) + 1);
+
+	if (ptr == NULL)
 		return (NULL);
 
-	for (y = 0; y < x; y++)
-		p[y] = str[y];
-	p[y] = '\0';
+	for (i = 0; i < size; i++)
+		ptr[i] = str[i];
 
-	return (p);
+	return (ptr);
 }
