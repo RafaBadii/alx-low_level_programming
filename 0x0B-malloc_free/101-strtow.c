cbdca3e56a6e84414620000af44bbe51f321c38b
RafaBadi <rafa.ibrahim.khalid@example.com>
2023-09-05 17:22:24 -0700

    Task has been completed

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,40 +1,39 @@
 #include "main.h"
 
 /**
- * wrdcnt - Function counts the number of words in a string.
- * @str: string to count.
- * Return: int of number of words.
+ * wrdcnt - Function counts number of words in string
+ * @str: string
+ * Return: int
 */
 
 int wrdcnt(char *str)
 {
-	int i, number = 0;
+	int i, n = 0;
 
 	for (i = 0; str[i]; i++)
 	{
 		if (str[i] == ' ')
 		{
 			if (str[i + 1] != ' ' && str[i + 1] != '\0')
-				number++;
+				n++;
 		}
 			else if (i == 0)
-				number++;
+				n++;
 	}
-	number++;
-	return (number);
+	n++;
+	return (n);
 }
 
-
 /**
- * strtow - Function split a string to words.
- * @str: string to split.
- * Return: pointer of the splited words.
+ * strtow - Function split a string to words
+ * @str: string
+ * Return: pointer
 */
 
 char **strtow(char *str)
 {
 	int i, j, x, y, num = 0, wc = 0;
-	char **words;
+	char **w;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
@@ -42,12 +41,11 @@ char **strtow(char *str)
 	num = wrdcnt(str);
 	if (num == 1)
 		return (NULL);
-
-	words = (char **)malloc(sizeof(char *));
-	if (words == NULL)
+	w = (char **)malloc(sizeof(char *));
+	if (w == NULL)
 		return (NULL);
 
-	words[num - 1] = NULL;
+	w[num - 1] = NULL;
 	i = 0;
 
 	while (str[i])
@@ -57,25 +55,25 @@ char **strtow(char *str)
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 				;
 			j++;
-			words[wc] = (char *)malloc(j * sizeof(char));
+			w[wc] = (char *)malloc(j * sizeof(char));
 
 			j--;
-			if (words[wc] == NULL)
+			if (w[wc] == NULL)
 			{
 				for (x = 0; x < wc; x++)
-					free(words[x]);
-				free(words[num - 1]);
-				free(words);
+					free(w[x]);
+				free(w[num - 1]);
+				free(w);
 			return (NULL);
 			}
 			for (y = 0; y < j; y++)
-				words[wc][y] = str[i + y];
-			words[wc][y] = '\0';
+				w[wc][y] = str[i + y];
+			w[wc][y] = '\0';
 			wc++;
 			i += j;
 		}
 		else
 			i++;
 	}
-	return (words);
+	return (w);
 }
